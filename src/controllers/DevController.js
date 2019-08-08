492cd3f3700ec5083ac29b0e5b017eace1ad6a0a
Rodrigo Rodrigues de Oliveira <rroliveira@hospitalonix.local>
2019-08-08 17:08:54 -0300

    Adicionado LikeController e DislikeController

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,12 +1,32 @@
 const axios = require('axios');
+const Dev = require('../models/Dev');
 
 module.exports = {
+
+    async index(req, res){
+
+    },
+
     async store(req, res) {
-        const { username} = req.body;
+        const { username } = req.body;
+
+        const userExist = await Dev.findOne({ user: username });
+
+        if(userExist) {
+            return res.json(userExist);
+        }
 
         const response = await axios.get(`https://api.github.com/users/${username}`);
 
+        const { name, bio, avatar_url: avatar } = response.data;
 
-        return res.json(response.data);
+        const dev = await Dev.create({
+            name,
+            user: username,
+            bio,
+            avatar
+        });
+        
+        return res.json(dev);
     }
 }
